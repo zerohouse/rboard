4b2d456ea0fdcd55e99c6d7a597930ece9fdb608
dev <parksungho86@gmail.com>
2015-07-04 04:19:07 +0900

    fa

--- app.js
+++ app.js
@@ -101,14 +101,46 @@ $mapping('post.write', function (post, response) {
     });
 });
 
-$mapping('post.search', function (post, response) {
-    db.post.insertOne(post, function (err, result) {
-        var res = {};
-        res.err = err;
-        res.result = result;
-        response(res);
+$mapping('post.search', function (keyword, response) {
+    db.post.aggregate([
+        {
+            $match: {
+                board: {
+                    $regex: ".*" + keyword + ".*"
+                }
+            }
+        },
+        {
+            $group: {
+                _id: "$board",
+                count: {$sum: 1}
+            }
+        },
+        {
+            $project: {
+                _id: 0,
+                board: "$_id",
+                count: 1
+            }
+        },
+        {$sort: {count: -1}},
+        {$limit: 5}
+    ], function (err, result) {
+        response(result);
     });
 });
+
+
+$mapping('post.get', function (req, response) {
+    db.post.find({board: req.board}, {
+        "limit": req.limit,
+        "skip": req.skip
+    }, function (err, result) {
+        response(result);
+    });
+});
+
+
 $mapping('user.register', function (user, response) {
     db.user.insertOne(user, function (err, result) {
         var res = {};
--- client/js/controllers/boardController.js
+++ client/js/controllers/boardController.js
@@ -1,3 +1,74 @@
-app.controller('boardController', function () {
+app.controller('boardController', function ($scope, $req, $state, $stateParams) {
+
+    $scope.keyword = "";
+
+    $scope.$watch('keyword', function () {
+        if ($scope.keyword == undefined || $scope.keyword == "") {
+            $scope.results = [];
+            return;
+        }
+        $req('post.search', $scope.keyword, function (res) {
+            if (!check()) {
+                var now = {};
+                now.count = 0;
+                now.board = $scope.keyword;
+                res.push(now)
+            }
+            $scope.results = res;
+            $scope.select = res[0];
+            $scope.$apply();
+            function check() {
+                for (var i = 0; i < res.length; i++) {
+                    if (res[i].board == $scope.keyword)
+                        return true;
+                }
+                return false;
+            }
+        });
+    });
+
+    $scope.$watch(function () {
+        return $stateParams.url;
+    }, function () {
+        var req = {};
+        req.board = $stateParams.url;
+        req.limit = 5;
+        req.skip = 0;
+        $req('post.get', req, function (res) {
+            $scope.titles = res;
+            console.log(res);
+        });
+    });
+
+    $scope.selectResult = function (select) {
+        $scope.select = select;
+    };
+
+    $scope.move = function () {
+        $state.go('board', {url: $scope.select.board});
+    };
+
+    $scope.keyPress = function (e) {
+        if ($scope.results.length == 0)
+            return;
+        var index = $scope.results.indexOf($scope.select);
+        switch (e.keyCode) {
+            case 38:
+                index--;
+                if (index < 0)
+                    index = $scope.results.length - 1;
+                $scope.select = $scope.results[index];
+                break;
+            case 40:
+                index++;
+                if (index > $scope.results.length - 1)
+                    index = 0;
+                $scope.select = $scope.results[index];
+                break;
+            case 13:
+                $scope.move();
+                break;
+        }
+    }
 
 });
